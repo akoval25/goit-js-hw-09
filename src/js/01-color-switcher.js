f73614f1109bdf55421d46d967d7ccbb97ad341a
akoval25 <88685936+akoval25@users.noreply.github.com>
2022-02-12 16:54:23 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let intervalId = 0;
+
+const onStart = (e) => {
+  e.preventDefault();
+  intervalId = setInterval(getRandomHexColor, 1000);
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+}
+
+const onStop = (e) => {
+  e.preventDefault();
+  clearInterval(intervalId);
+  btnStop.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+}
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+
+function getRandomHexColor() {
+  return body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
